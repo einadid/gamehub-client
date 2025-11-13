4b13d5d0d6f4810935c50046117ad30d6b249109
einadid <emamulislamnadid_cse30d@portcity.edu.bd>
2025-11-13 10:50:48 -0800

    fix: add target and rel attributes to footer link for improved accessibility

--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -7,8 +7,8 @@ const Footer = () => {
     return (
         <footer className="footer footer-center p-4 bg-base-300 text-base-content font-montserrat">
             <aside>
-                {/* এখানে ডাইনামিক currentYear ভ্যারিয়েবলটি ব্যবহার করা হচ্ছে */}
-                <p>Copyright © {currentYear} - All right reserved by <a href="https://github.com/einadid">einadid</a></p>
+                {/* এখানে ডাইনামিক currentYear ভ্যারিয়েবলটি ব্যবহার করা হচ্ছে */}
+                <p>Copyright © {currentYear} - All right reserved by <a href="https://github.com/einadid" target="_blank" rel="noopener noreferrer">einadid</a></p>
             </aside>
         </footer>
     );
